3306ff67599b456bc65852a110b7cb641817de81
theusodev <matheuspiudeoliveira@gmail.com>
2024-04-10 10:32:51 -0300

    tela login finalizada

--- lib/features/login/login_page.dart
+++ lib/features/login/login_page.dart
@@ -1,7 +1,13 @@
 import 'dart:ffi';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'dart:ui' as ui;
+
+import 'package:flutter/widgets.dart';
+import 'package:unasp_fome_app/features/home.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -52,52 +58,64 @@ class LoginPage extends StatelessWidget {
                     ),
                   ),
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    // Adicione aqui a lógica de autenticação
-                    // Por exemplo, usando Firebase Authentication
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFF9944A)),
-                    textStyle: MaterialStateProperty.all<TextStyle>(TextStyle(color: Colors.white))
-                  ),
-                  child: Text('Entrar',
-                  style: TextStyle(color: Colors.white),
-                  ),
-                ),
+                
+                //BOTÃO ENTRAR
+                MaterialButton( 
+                  height: 41.0, 
+                  minWidth: 171.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100)),
+                  color: (Color(0xFFF9944A)),
+                  //color: Theme.of(context).primaryColor, 
+                  textColor: Colors.white, 
+                  child: new Text("Entrar"), 
+                  onPressed: () => {}, 
+                  //splashColor: Colors.redAccent,
+                )
               ],
             ),
-            SizedBox(height: 8.0),
+            SizedBox(height: 0.0),
             
-            ElevatedButton(
-              onPressed: () {
-                // Navegue para a tela de registro
-                // Por exemplo, Navigator.push(context, MaterialPageRoute(builder: (context) => RegistrationPage()));
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFF9944A)), // Define a cor do texto
-                
-              ),
-              child: Text('Cadastre-se',
-              style: TextStyle(color: Colors.white),
-              ),
-            ),
-            SizedBox(height: 8.0),
+                //NOVO BOTÃO CADASTRAR-SE
+                MaterialButton( 
+                  height: 41.0, 
+                  minWidth: 171.0, 
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100)),
+                  color: (Color(0xFFF9944A)),
+                  //color: Theme.of(context).primaryColor, 
+                  textColor: Colors.white, 
+                  child: new Text("Cadastrar-se"), 
+                  onPressed: () => {}, 
+                  //splashColor: Colors.redAccent,
+                ),
+                SizedBox(height: 10.0),
 
-            ElevatedButton.icon(
-              onPressed: () {
-                // Adicione aqui a lógica para o login com Google
-              },
-              icon: Image.asset('assets/images/Google_Login.png', width: 24, height: 24), // Ícone do Google
-              label: Text('Entrar com o Google'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-              ),
-            ),
+                //botão google
+                MaterialButton(
+                  onPressed: () {
+                    //lógica do botão aqui
+                  },
+                  height: 41.0,
+                  minWidth: 171.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100)),
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  textColor: const Color.fromARGB(255, 0, 0, 0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Image.asset('assets/images/Google_Login.png',
+                      width: 24,
+                      height: 24,
+                      ),
+                      SizedBox(width: 8),
+                      Text('Entrar com o Google')
+                    ],
+                  ),
+                ),
           ],
         ),
-        
       ),
     );
   }
