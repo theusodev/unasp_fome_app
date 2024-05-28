015d0b68e1cc9d51be9f00f7c9165868c61b337a
theusodev <matheuspiudeoliveira@gmail.com>
2024-05-28 16:25:02 -0300

    construção de checkout

--- lib/view/cart_page.dart
+++ lib/view/cart_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:unasp_fome_app/model/cart_model.dart';
+import 'package:unasp_fome_app/view/checkout_page.dart';
 
 class Cartpage extends StatelessWidget {
   const Cartpage({super.key});
@@ -8,100 +9,119 @@ class Cartpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //essa é a tela de carrinho
-      //nome "meu carrinho" no topo da tela
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Text("Meu carrinho",
-        style: TextStyle(fontSize: 36,
-        fontWeight: FontWeight.bold),),
-      ),
-      
-      //consumindo a classe do CartModel onde armazena os itens de carrinho
-      body: Consumer<CartModel>(builder: (context, value, child) {
-        //aqui inicia a lista de produtos quando adicionados
-        return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          //lista de itens do carrinho
-          Expanded(child: ListView.builder(
-            itemCount: value.itensCart.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(16),
-                    border: Border.all(color: Colors.black, width: 1)
-                  ),
-                  child: ListTile(
-                    leading: Image.asset(value.itensCart[index][2], height: 36,),
-                    title: Text(value.itensCart[index][0]),
-                    subtitle: Text('R\$ ' + value.itensCart[index][1]),
-                    trailing: IconButton(icon: Icon(Icons.cancel),
-                    onPressed: () => 
-                    Provider.of<CartModel>(context, listen: false)
-                    .removeItens(index)),
-                  ),
-                ),
-              );
-            })),
+        //essa é a tela de carrinho
+        //nome "meu carrinho" no topo da tela
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Text(
+            "Meu carrinho",
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          ),
+        ),
 
-            //botão cinza de valor total no final da tela
-            Padding(
-              padding: const EdgeInsets.all(36.0),
-              child: Container(
-                decoration: BoxDecoration(color: Colors.grey,
-                borderRadius: BorderRadius.circular(8)),
-                padding: EdgeInsets.all(24),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    //preço
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+        //consumindo a classe do CartModel onde armazena os itens de carrinho
+        body: Consumer<CartModel>(
+          builder: (context, value, child) {
+            //aqui inicia a lista de produtos quando adicionados
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //lista de itens do carrinho
+                Expanded(
+                    child: ListView.builder(
+                        itemCount: value.itensCart.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  borderRadius: BorderRadius.circular(16),
+                                  border: Border.all(
+                                      color: Colors.black, width: 1)),
+                              child: ListTile(
+                                leading: Image.asset(
+                                  value.itensCart[index][2],
+                                  height: 36,
+                                ),
+                                title: Text(value.itensCart[index][0]),
+                                subtitle:
+                                    Text('R\$ ' + value.itensCart[index][1]),
+                                trailing: IconButton(
+                                    icon: Icon(Icons.cancel),
+                                    onPressed: () => Provider.of<CartModel>(
+                                            context,
+                                            listen: false)
+                                        .removeItens(index)),
+                              ),
+                            ),
+                          );
+                        })),
+
+                //botão cinza de valor total no final da tela
+                Padding(
+                  padding: const EdgeInsets.all(36.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(8)),
+                    padding: EdgeInsets.all(24),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text("Valor total",
-                        style: TextStyle(color: Colors.black,
-                        fontSize: 14)),
-                        
-                        SizedBox(height: 4),
+                        //preço
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Valor total",
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 14)),
+                            SizedBox(height: 4),
+                            Text(
+                              "R\$ " + value.calcularTotal(),
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
 
-                        Text("R\$ " + value.calcularTotal(),
-                        style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold
-                        ),)
-
+                        Container(
+                          padding: EdgeInsets.all(12),
+                          child: Row(
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              CheckoutPage()));
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.grey,
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16),
+                                  side: BorderSide(
+                                    color: Colors.black, width: 1
+                                  )),
+                                ),
+                                child: Text(
+                                  "Pagar",
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
                       ],
                     ),
-
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black),
-                        borderRadius: BorderRadius.circular(12)
-                      ),
-                      padding: EdgeInsets.all(12),
-                      child: Row(
-                        children: const [
-                          Text("Pagar",
-                          style: TextStyle(color: Colors.black),),
-                          Icon(Icons.arrow_forward_ios,
-                          size: 16,
-                          color: Colors.black,)
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
-        ],
-      );
-      },)
-    );
+                  ),
+                )
+              ],
+            );
+          },
+        ));
   }
 }
---
+++ lib/view/checkout_page.dart
@@ -0,0 +1,267 @@
+import 'dart:js_util';
+
+import 'package:flutter/material.dart';
+
+class CheckoutPage extends StatefulWidget {
+  const CheckoutPage({super.key});
+
+  @override
+  State<CheckoutPage> createState() => _CheckoutPageState();
+}
+
+class _CheckoutPageState extends State<CheckoutPage> {
+  int _type = 1;
+
+  void _handleRadio(Object? e) => setState(() {
+        _type = e as int;
+      });
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pagamento"),
+      ),
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                width: size.width,
+                height: 55,
+                //margin: EdgeInsets.only(right: 20),
+                decoration: BoxDecoration(
+                    border: _type == 1
+                        ? Border.all(width: 1, color: Colors.red)
+                        : Border.all(width: 0.3, color: Colors.grey),
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.transparent),
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Radio(
+                              value: 1,
+                              groupValue: _type,
+                              onChanged: _handleRadio,
+                              activeColor: Colors.red,
+                              ),
+                              Text("Débito",
+                              style: _type == 1
+                                ? TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black)
+                                  : TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey
+                                )
+                              ),
+                              
+                        ],
+                      ),
+                      Icon(Icons.credit_card)
+                    ],
+                  ),
+                ),
+              ),
+              
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: size.width,
+                height: 55,
+                //margin: EdgeInsets.only(right: 20),
+                decoration: BoxDecoration(
+                    border: _type == 2
+                        ? Border.all(width: 1, color: Colors.red)
+                        : Border.all(width: 0.3, color: Colors.grey),
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.transparent),
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Radio(
+                              value: 2,
+                              groupValue: _type,
+                              onChanged: _handleRadio,
+                              activeColor: Colors.red,
+                              ),
+                              Text("Crédito",
+                              style: _type == 2
+                                ? TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black)
+                                  : TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey
+                                )
+                              ),
+                              
+                        ],
+                      ),
+                      Icon(Icons.credit_card)
+                    ],
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: size.width,
+                height: 55,
+                //margin: EdgeInsets.only(right: 20),
+                decoration: BoxDecoration(
+                    border: _type == 3
+                        ? Border.all(width: 1, color: Colors.red)
+                        : Border.all(width: 0.3, color: Colors.grey),
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.transparent),
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Radio(
+                              value: 3,
+                              groupValue: _type,
+                              onChanged: _handleRadio,
+                              activeColor: Colors.red,
+                              ),
+                              Text("Dinheiro",
+                              style: _type == 3
+                                ? TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black)
+                                  : TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey
+                                )
+                              ),
+                              
+                        ],
+                      ),
+                      Icon(Icons.money)
+                    ],
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: size.width,
+                height: 55,
+                //margin: EdgeInsets.only(right: 20),
+                decoration: BoxDecoration(
+                    border: _type == 4
+                        ? Border.all(width: 1, color: Colors.red)
+                        : Border.all(width: 0.3, color: Colors.grey),
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.transparent),
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Radio(
+                              value: 4,
+                              groupValue: _type,
+                              onChanged: _handleRadio,
+                              activeColor: Colors.red,
+                              ),
+                              Text("Pix",
+                              style: _type == 4
+                                ? TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black)
+                                  : TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey
+                                )
+                              ),
+                              
+                        ],
+                      ),
+                      Icon(Icons.qr_code)
+                    ],
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 100,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Total: ",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey
+                    ),
+                  ),
+                  Text(
+                    "100",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black
+                    ),
+                  ),
+                ],
+              ),
+
+            //parei a construçã aqui
+            //video referencia: https://www.youtube.com/watch?v=RERkUPqh71Y
+
+
+
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
