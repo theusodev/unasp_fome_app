0afb803f02befb94f2f2f249aa99a28761d02018
theusodev <matheuspiudeoliveira@gmail.com>
2024-06-09 23:57:02 -0300

    mensagem de item não encontrado e sem produtos no histórico adicionado

--- lib/view/cart_page.dart
+++ lib/view/cart_page.dart
@@ -168,40 +168,49 @@ class HistoricoPedidosPage extends StatelessWidget {
       ),
       body: Consumer<CartModel>(
         builder: (context, cartModel, child) {
-          return ListView.builder(
-            itemCount: cartModel.historicoPedidos.length,
-            itemBuilder: (context, index) {
-              var pedido = cartModel.historicoPedidos[index];
-              var precoTotal = cartModel.calcularTotalPedido(pedido);
-              return GestureDetector(
-                onTap: () {
-                  cartModel.refazerPedido(pedido);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CartPage(),
-                    ),
-                  );
-                },
-                child: ExpansionTile(
-                  title: Text("Pedido ${index + 1} - Total: R\$ $precoTotal"),
-                  children: pedido.entries.map<Widget>((entry) {
-                    var item = entry.key;
-                    var quantidade = entry.value;
-                    return ListTile(
-                      leading: Image.asset(
-                        item[2],
-                        height: 36,
+          if (cartModel.historicoPedidos.isEmpty) {
+            return Center(
+              child: Text(
+                "Sem produtos no histórico",
+                style: TextStyle(fontSize: 18, color: Colors.black),
+              ),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: cartModel.historicoPedidos.length,
+              itemBuilder: (context, index) {
+                var pedido = cartModel.historicoPedidos[index];
+                var precoTotal = cartModel.calcularTotalPedido(pedido);
+                return GestureDetector(
+                  onTap: () {
+                    cartModel.refazerPedido(pedido);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CartPage(),
                       ),
-                      title: Text(item[0]),
-                      subtitle: Text('R\$ ${item[1]}'),
-                      trailing: Text('Qtd: $quantidade'),
                     );
-                  }).toList(),
-                ),
-              );
-            },
-          );
+                  },
+                  child: ExpansionTile(
+                    title: Text("Pedido ${index + 1} - Total: R\$ $precoTotal"),
+                    children: pedido.entries.map<Widget>((entry) {
+                      var item = entry.key;
+                      var quantidade = entry.value;
+                      return ListTile(
+                        leading: Image.asset(
+                          item[2],
+                          height: 36,
+                        ),
+                        title: Text(item[0]),
+                        subtitle: Text('R\$ ${item[1]}'),
+                        trailing: Text('Qtd: $quantidade'),
+                      );
+                    }).toList(),
+                  ),
+                );
+              },
+            );
+          }
         },
       ),
     );
