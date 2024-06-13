6a78bc98f3f4735a14409be67a9867b4a592459d
theusodev <matheuspiudeoliveira@gmail.com>
2024-06-13 11:33:08 -0300

    feature esqueci minha senha finalizada

--- lib/view/esqueciminhasenha_page.dart
+++ lib/view/esqueciminhasenha_page.dart
@@ -6,7 +6,7 @@ class EsqueciminhasenhaPage extends StatelessWidget {
   final TextEditingController _emailController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
 
-    // Custom input decoration
+  // Custom input decoration
   InputDecoration inputDecoration(String label) {
     return InputDecoration(
       labelText: label,
@@ -22,10 +22,32 @@ class EsqueciminhasenhaPage extends StatelessWidget {
 
   void _resetPassword(BuildContext context) async {
     try {
-      await FirebaseAuth.instance.sendPasswordResetEmail(email: _emailController.text);
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: _emailController.text);
+      _showAlertDialog(context, 'Sucesso', 'E-mail de recuperação enviado com sucesso. Verifique sua caixa de entrada.');
     } catch (e) {
-      print("Error: $e");
+      _showAlertDialog(context, 'Erro', 'Falha ao enviar o e-mail de recuperação. Verifique o e-mail e tente novamente.');
     }
+  }
+
+  void _showAlertDialog(BuildContext context, String title, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -39,37 +61,49 @@ class EsqueciminhasenhaPage extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
-
       body: Padding(
         padding: EdgeInsets.all(16),
         child: Column(
           children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Flexible(
+                  child: Text(
+                    "Insira seu e-mail e verifique a caixa de entrada para restaurar sua senha.",
+                    style: TextStyle(fontSize: 16,
+                    fontWeight: FontWeight.w700),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 12),
             TextFormField(
               controller: _emailController,
               decoration: inputDecoration("E-mail"),
               validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Insira um e-mail";
-                    }
+                if (value!.isEmpty) {
+                  return "Insira um e-mail";
+                }
 
-                    if (!value.contains("@")) {
-                      return 'Insira um e-mail válido';
-                    }
-                    return null;
-                  },
+                if (!value.contains("@")) {
+                  return 'Insira um e-mail válido';
+                }
+                return null;
+              },
             ),
-
             SizedBox(height: 16),
-
-            ElevatedButton(onPressed: () => _resetPassword(context), 
-            child: Text("Reenviar senha",
-            style: TextStyle(color: Colors.white),),
-            style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.orange),
+            ElevatedButton(
+              onPressed: () => _resetPassword(context),
+              child: Text(
+                "Reenviar senha",
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.orange),
             ),
-
           ],
-        ),),
+        ),
+      ),
     );
   }
 }
